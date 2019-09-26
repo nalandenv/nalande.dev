26228a57c3a1e51cce33269c265a33d99ad86f32
Nikhil Nalande <nalandenv@gmail.com>
2019-09-26 16:40:38 +0530

    add about me

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ var express = require("express"),
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+// home directory
 app.get("/", function(req, res){
     res.render("home");
+});
+
+//about me
+app.get("/about_me", function(req, res){
+    res.render("about_me");
 });
 
 app.listen(process.env.PORT, process.env.IP);
